9b4ba6d1106d6d1cbf16799e28467bc12447979c
Ayesha Latif <latifayesha@yahoo.com>
2022-11-23 12:03:46 -0500

    First 3 Prompts

---
+++ index.js
@@ -0,0 +1,19 @@
+//Problem1
+console.log("------Write a for loop that logs 0-15------")
+for (i = 0; i <= 15; i++)
+  console.log(i)
+console.log("********************************")
+
+//Problem 2
+console.log("------Write a for loop that counts from 15-0----")
+for (i = 15; i >= 0; i--)
+  console.log(i)
+console.log("********************************")
+
+//Problem 3
+console.log("------------- Make variable laps = 0; write a for loop that logs (“that’s another lap!”) and adds 1 to the laps variable. AFTER the loop, log laps.---------------")
+let laps = 0
+for (i = 0; i < 25; i++)
+  laps += 1;
+console.log("Number of Laps =", laps)
+console.log("********************************")
